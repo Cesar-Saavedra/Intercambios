9b3ae2e90251374deca726b10939de720db80ef3
César Saavedra C. <ces.saavedra@duocuc.cl>
2026-05-09 17:08:24 -0400

    Cesar
    Primer Commit

---
+++ src/main/java/cl/duoc/ms_intercambio/MsIntercambioApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_intercambio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIntercambioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIntercambioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_intercambio/MsIntercambioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_intercambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIntercambioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
